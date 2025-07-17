906da18e50e0d3b04900ab6e57770740e3bdc919
patrick <patrick.mcisaac.dev@gmail.com>
2025-07-17 09:47:01 -0500

    fire pottery function working

---
+++ src/scripts/Kiln.js
@@ -0,0 +1,10 @@
+export const firePottery = (obj, temp) => {
+    obj.fired = true
+    if(temp > 2200) {
+        obj.cracked = true
+    }else {
+        obj.cracked = false
+    }
+
+    return obj
+} 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 // Imports go first
 import { makePottery } from "./PotteryWheel.js"
+import { firePottery } from "./Kiln.js"
 
 
 // Make 5 pieces of pottery at the wheel
@@ -9,9 +10,12 @@ let plate = makePottery('plate', 1, .5)
 let bowl = makePottery('bowl', 2, 3)
 let cup = makePottery('cup', .4, 2)
 
-
 // Fire each piece of pottery in the kiln
-
+const firedMug = firePottery(mug, 2400)
+const firedVase = firePottery(vase, 2000)
+const firedPlate = firePottery(plate, 1400)
+const firedBowl = firePottery(bowl, 3400)
+const firedCup = firePottery(cup, 2200)
 
 // Determine which ones should be sold, and their price
 
